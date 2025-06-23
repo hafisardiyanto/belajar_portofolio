b28f51489e2f842b157f72463218d78806d9a0d8
Hafis Ardiyanto <hafisardiyanto19@gmail.com>
2025-06-23 21:39:23 +0700

    awalan pertama

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import router from '@/router'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  router,
 }
 </script>
+
+
 
 <style>
 #app {
