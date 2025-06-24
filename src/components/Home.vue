1a71e85c5abc50ab2aa5760bb82b6b537b94f960
Hafis Ardiyanto <hafisardiyanto19@gmail.com>
2025-06-24 11:19:21 +0700

    buat route di index js untuk halaman home

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Selamat Datang di Halaman Home</h1>
+    <p>Ini adalah halaman utama portofolio Anda.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
+import HelloWorld from '../components/Home.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HelloWorld
-  }
+    },
+    
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+
+  
 ]
 
 const router = createRouter({
