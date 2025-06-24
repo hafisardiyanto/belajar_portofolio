20018c7bfbb1e341293332a9ba73a4e4c7349c4b
Hafis Ardiyanto <hafisardiyanto19@gmail.com>
2025-06-24 14:24:58 +0700

    nav di home vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="home-page">
-    <h1>Selamat Datang di Halaman Home</h1>
-    <p>Ini adalah halaman utama portofolio Anda.</p>
-    <th>Hy</th>
+
+    <h1>Selamat Datang di Halaman Home portofolio</h1>
+
+    <h2>Halo, Saya Hafis Ardiyanto!</h2>
+
+    <h3>Selamat datang di portofolio saya seorang programmer yang berfokus framework laravel </h3>
+
+    <ul>
+      <li>👨‍💻 Pengalaman membangun aplikasi web modern</li>
+      <li>🚀 Terbiasa dengan pengembangan Frontend & Backend</li>
+      <li>📚 Suka belajar teknologi baru</li>
+    </ul>
+
+     <nav>
+    <ul>
+      <li><a href="/home">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </nav>
+  
   </div>
 </template>
 
@@ -22,7 +40,21 @@ export default {
   /* Gradasi cerah */
   background: linear-gradient(135deg, #f6d365 0%, #fda085 100%);
 }
-h1, p {
+
+h1 {
   color: #333;
 }
+
+h2 {
+  margin-top: 0;
+  color: #ff6a00;
+}
+
+ul {
+  text-align: left;
+  margin: 1em auto;
+  max-width: 350px;
+  color:rgb(250, 246, 14)
+}
+
 </style>
