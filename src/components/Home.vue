56b7bdbbc2f2c5deaec52c1571bb84b603105ef3
Hafis Ardiyanto <hafisardiyanto19@gmail.com>
2025-06-24 14:26:58 +0700

    menghapus coding nav

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,14 +13,8 @@
       <li>📚 Suka belajar teknologi baru</li>
     </ul>
 
-     <nav>
-    <ul>
-      <li><a href="/home">Home</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/contact">Contact</a></li>
-    </ul>
-  </nav>
-  
+    
+
   </div>
 </template>
 
